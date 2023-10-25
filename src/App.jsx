37f8245c890f084432ec2e9ee94df13303e95df8
bhairav1999 <bhairavkarande1999@gmail.com>
2023-10-25 22:57:21 +0530

    Implementing React Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+import Contact from "./components/contact/Contact";
+import Product from "./components/product/Product";
+import SingleProduct from "./components/singleProduct/SingleProduct";
+import Cart from "./components/cart/Cart";
+import ErrorPage from "./components/errorPage/ErrorPage";
 
 function App() {
   return (
     <>
-   <div className="wrapper">
-   hello
-   </div> 
-    
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/product" element={<Product />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+          <Route path="/singleproduct/:id" element={<SingleProduct />}></Route>
+          <Route path="/*" element={<ErrorPage />}></Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
 export default App;
